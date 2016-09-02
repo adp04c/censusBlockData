ad219cf794299d27e1911e665cb6abd07cd82bb9
adp04c <adp04c@gmail.com>
2016-09-02 10:04:32 -0400

    Create example.py

---
+++ example.py
@@ -0,0 +1,19 @@
+from census import censusData
+
+#initiate
+a = census(28.35975,-81.421988)
+
+#get all data
+a.data()
+
+#get census FIPS block number
+a.block()
+
+#get adjacent block numbers
+a.intersection()
+
+#get state 
+a.state()
+
+#get county
+a.county()
